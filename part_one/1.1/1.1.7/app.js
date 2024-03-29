65911286a0f25a6e8976d4e954fbc2c4c26060b4
manu-cj <carenjotje@gmail.com>
2024-03-29 14:34:55 +0100

    1.1.7 finish

---
+++ part_one/1.1/1.1.7/app.js
@@ -0,0 +1,15 @@
+
+
+let sum = 0;
+let iteration = 1;
+
+function choiceNumber(n){
+    n = parseInt(window.prompt("Choisissez un nombre"));
+    for (let i = 0; i < n; i++) {
+        n = parseInt(window.prompt("Choisissez encore un nombre"));
+        sum += n;
+    }
+    console.log(sum);
+}
+
+choiceNumber();
